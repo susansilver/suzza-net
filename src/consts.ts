e692fafb5141f1700e0adc0aa7e3e42337f4ab85
Suzza <hello+git@susansilver.net>
2023-07-29 13:02:11 -0700

    removed styles add light/dark theme toggle

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Suzza's Infinite Playlist";
+export const SITE_DESCRIPTION = 'Add new desc';
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	daisyui: {
+		themes: [ 
+		  {
+			mytheme: {
+			
+   "primary": "#1565C0",
+			
+   "secondary": "#EC407A",
+			
+   "accent": "#26A694",
+			
+   "neutral": "#191D24",
+			
+   "base-100": "#90CAF9",
+			},
+		  },
+	"synthwave",
+		],
+	  },
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
