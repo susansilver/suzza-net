9995481835271def529827e3d3b40e38875ceee9
Suzza <hello+git@susansilver.net>
2023-07-31 11:31:24 -0700

    fix color schema and add text to hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,10 +15,15 @@ module.exports = {
    "neutral": "#EC407A",
 			
    "base-100": "#fecdd3",
+
+   "white": "#f1f5f9",
 			},
 		  },
+	"black",
 	"synthwave",
+	"night"
 		],
+	darkTheme: "synthwave",
 	  },
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
