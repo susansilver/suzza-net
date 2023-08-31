8d06933cd75d7102dbb96cc15aa4e2f399f01048
Suzza <hello+git@susansilver.net>
2023-08-31 08:56:29 -0700

    update astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-   assets: true
-  },
   site: 'https://suzza.net',
   integrations: [mdx(), sitemap(), tailwind()]
 });
